53136bc6f12a5d038fed2f30212f35f4bd43912f
danielalariksson01 <daniel.alariksson@gmail.com>
2025-04-07 11:55:50 +0200

    okok

--- test_drive.py
+++ test_drive.py
@@ -1,3 +1,10 @@
 print("jag kan inte git")
 
 
+for i in range(10):
+    print(i)
+    print("jag kan inte git")           
+
+print("jag kan inte git")
+print("jag kan inte git")
+print("jag kan inte git")
